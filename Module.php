5a66428c0f548cd3f8391ce03673b32d3150b49d
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-02-16 21:22:51 -0500

    Update Module.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- Module.php
+++ Module.php
@@ -4,6 +4,11 @@ namespace humhub\modules\composer;
 
 class Module extends \humhub\components\Module
 {
+    /**
+     * @inheritdoc
+     */
+    public $resourcesPath = 'resources';
+
     public function getConfigUrl()
     {
         return '/composer/composer/index';
