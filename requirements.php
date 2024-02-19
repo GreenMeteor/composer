c1d07e796a044c45005ee2654a41a5ae1b35b678
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-02-19 17:31:19 -0500

    Create requirements.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

---
+++ requirements.php
@@ -0,0 +1,24 @@
+<?php
+
+$requirements = [];
+
+if (PHP_VERSION_ID < 80100) {
+    $requirements[] = 'Please upgrade to PHP Version 8.1 or later!';
+}
+
+if (!extension_loaded('mbstring')) {
+    $requirements[] = 'You need to enable MBString extension in your PHP configuration on the server.';
+}
+
+if (!extension_loaded('json')) {
+    $requirements[] = 'You need to enable JSON extension in your PHP configuration on the server.';
+}
+
+// Check directory writability
+$directory = 'protected/vendor';
+$fullPath = Yii::getAlias('@app') . '/' . $directory;
+if (!is_writable($fullPath)) {
+    $requirements[] = "The directory '$directory' is not writable by the PHP process.";
+}
+
+return $requirements;
