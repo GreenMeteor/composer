d891be014069edeba5a3793cf961a3223db2342a
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-02-20 14:21:03 -0500

    Update index.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- views/composer/index.php
+++ views/composer/index.php
@@ -1,13 +1,16 @@
 <?php
 
 use humhub\modules\ui\form\widgets\ActiveForm;
+use humhub\modules\ui\icon\widgets\Icon;
+use humhub\widgets\Button;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 ?>
-
 <div class="container">
     <div class="panel panel-default">
         <div class="panel-heading">
+            <?= Button::asLink(Icon::get('git'))->link([Url::to('/composer/git/pull')])->cssClass('pull-right btn btn-default')->tooltip('Git') ?>
             <?= Html::tag('h1', 'Composer Update'); ?>
         </div>
         <div class="panel-body">
