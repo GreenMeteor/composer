e07c360e0260886442e08f50132c505686600da0
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-04-08 02:16:33 -0400

    Update config.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 
 use humhub\modules\composer\Module;
+use humhub\modules\composer\Events;
+use humhub\widgets\TopMenu;
 
 return [
     'id' => 'composer',
     'class' => Module::class,
     'namespace' => 'humhub\modules\composer',
     'events' => [
+        ['class' => TopMenu::class, 'event' => TopMenu::EVENT_INIT, 'callback' => [Events::class, 'onTopMenuInit']],
     ],
 ];
