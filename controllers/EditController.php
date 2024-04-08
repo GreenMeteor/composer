87bf225d3c006967a684300c233970d8d72fd6be
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-04-08 02:12:58 -0400

    Create EditController.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

---
+++ controllers/EditController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace humhub\modules\composer\controllers;
+
+use Yii;
+use yii\helpers\Url;
+use yii\web\NotFoundHttpException;
+use humhub\modules\composer\models\EditForm;
+use humhub\modules\admin\components\Controller;
+
+/**
+ * EditController handles the editing of composer.json file.
+ */
+class EditController extends Controller
+{
+    /**
+     * Action to open the composer.json file in a modal.
+     * @throws \yii\web\NotFoundHttpException if the composer.json file is not found
+     * @return string|\yii\web\Response the rendering result
+     */
+    public function actionComposer()
+    {
+        $composerJsonFile = $_SERVER['DOCUMENT_ROOT'] . '/composer.json';
+
+        if (!file_exists($composerJsonFile)) {
+            throw new NotFoundHttpException('composer.json file not found.');
+        }
+
+        $model = new EditForm();
+        $model->composerData = file_get_contents($composerJsonFile);
+
+        if ($model->load(Yii::$app->request->post()) && $model->saveComposerData()) {
+            return $this->redirect(Url::home());
+        }
+
+        return $this->renderAjax('composer', [
+            'model' => $model,
+        ]);
+    }
+}
