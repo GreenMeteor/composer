faca7adc2134dd1e27674d9cbb2d4390797c9bb7
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-04-18 18:37:56 -0400

    Update pull.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- views/git/pull.php
+++ views/git/pull.php
@@ -9,24 +9,12 @@ $this->params['breadcrumbs'][] = $this->title;
 // Include requirements.php to access $requirements
 $requirements = require Yii::getAlias('@composer/requirements.php');
 ?>
-
 <div class="panel panel-default">
     <div class="panel-heading">
         <?= Html::tag('h1', Html::encode($this->title)); ?>
     </div>
 
     <div class="panel-body">
-        <?php if (!empty($requirements)): ?>
-            <div class="alert alert-danger" role="alert">
-                <strong>Error!</strong> Your server does not meet the following requirements:
-                <ul>
-                    <?php foreach ($requirements as $requirement): ?>
-                        <li><?= Html::encode($requirement) ?></li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
-        <?php endif; ?>
-
         <?php if (Yii::$app->session->hasFlash('success')): ?>
             <div class="alert alert-success" role="alert">
                 <?= Yii::$app->session->getFlash('success') ?>
@@ -46,8 +34,11 @@ $requirements = require Yii::getAlias('@composer/requirements.php');
             </div>
         <?php endif; ?>
 
-        <?= Html::beginForm(['perform-pull'], 'post') ?>
+        <?= Html::beginForm(['pull'], 'post') ?>
+            <?= Html::dropDownList('branch', 'master', ['master' => 'Master', 'develop' => 'Develop']) ?>
+            <br>
             <?= Html::submitButton('Pull from Git Repository', ['class' => 'btn btn-primary']) ?>
+            <?= Html::hiddenInput(Yii::$app->request->csrfParam, Yii::$app->request->getCsrfToken()) ?>
         <?= Html::endForm() ?>
     </div>
 </div>
