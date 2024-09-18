5a3769ad63469474980ec2338bf6eca70bc041b6
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-09-18 17:15:02 -0400

    Update ComposerController.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- controllers/ComposerController.php
+++ controllers/ComposerController.php
@@ -2,69 +2,25 @@
 
 namespace humhub\modules\composer\controllers;
 
-use Yii;
-use yii\helpers\Url;
-use Composer\Factory;
-use Composer\IO\BufferIO;
-use Symfony\Component\Console\Input\ArrayInput;
 use humhub\modules\admin\components\Controller;
-use Symfony\Component\Console\Output\BufferedOutput;
+use humhub\modules\composer\services\ComposerService;
 
-require_once(Yii::getAlias('@composer') . '/vendor' . '/autoload.php');
-
+/**
+ * Controller to handle Composer-related actions.
+ */
 class ComposerController extends Controller
 {
+    /**
+     * Default action to run a Composer command.
+     *
+     * @return string
+     */
     public function actionIndex()
     {
-        // Set the HOME environment variable to the user's home directory
-        putenv('HOME=' . $_SERVER['DOCUMENT_ROOT']);
+        $composerService = new ComposerService();
+        $command = 'install';
+        $output = $composerService->runComposerCommand($command);
 
-        // Assume $someCondition and $anotherCondition are defined elsewhere
-        $someCondition = true;
-        $anotherCondition = false;
-
-        // Determine the command based on some condition or input
-        $command = 'self-update';
-
-        // Condition to determine the command
-        if ($someCondition) {
-            $command = 'install';
-        } elseif ($anotherCondition) {
-            $command = 'update';
-        }
-
-        // Prepare input with the determined command
-        $inputOptions = ['command' => $command];
-
-        // Create an instance of BufferIO
-        $io = new BufferIO();
-
-        // Load Composer application
-        $composer = Factory::create($io, Url::to('composer.json'));
-
-        // Create Composer Application instance
-        $application = new \Composer\Console\Application();
-        $application->setAutoExit(false);
-
-        // Prepare input and output
-        $input = new ArrayInput($inputOptions);
-        $output = new BufferedOutput();
-
-        // Run the Composer command
-        $application->run($input, $output);
-
-        // Get the output of the command
-        $outputText = $output->fetch();
-
-        // Clear Composer cache
-        $composerCacheDir = getenv('COMPOSER_HOME') ?: (getenv('HOME') . DIRECTORY_SEPARATOR . '.composer');
-        $cacheFiles = glob($composerCacheDir . '/cache/*');
-        foreach ($cacheFiles as $cacheFile) {
-            if (is_file($cacheFile)) {
-                unlink($cacheFile);
-            }
-        }
-
-        return $this->render('index', ['output' => $outputText]);
+        return $this->render('index', ['output' => $output]);
     }
 }
