95a193af982c808775ea9179b5b778128836280e
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-09-20 19:32:34 -0400

    Update GruntController.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- controllers/GruntController.php
+++ controllers/GruntController.php
@@ -4,16 +4,25 @@ namespace humhub\modules\composer\controllers;
 
 use Yii;
 use humhub\modules\admin\components\Controller;
+use humhub\modules\composer\services\GruntService;
 
 /**
  * GruntController implements the actions for managing Grunt tasks.
  */
 class GruntController extends Controller
 {
+    private $gruntService;
+
+    public function __construct($id, $module, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->gruntService = new GruntService();
+    }
+
     /**
      * Displays the index page.
      *
-     * @return string the rendering result.
+     * @return string The rendering result.
      */
     public function actionIndex()
     {
@@ -23,14 +32,12 @@ class GruntController extends Controller
     /**
      * Builds assets using Grunt.
      *
-     * @return string the rendering result.
-     * @throws \yii\web\ForbiddenHttpException if the user is not allowed to perform this action.
-     * @throws \yii\web\ServerErrorHttpException if the working directory is invalid.
+     * @return string The rendering result.
      */
     public function actionBuildAssets()
     {
         try {
-            $output = $this->executeGruntCommand('build-assets');
+            $output = $this->gruntService->buildAssets();
             return $this->renderPartial('build-assets', ['output' => $output]);
         } catch (\Exception $e) {
             Yii::error('Error executing Grunt command: ' . $e->getMessage());
@@ -42,38 +49,32 @@ class GruntController extends Controller
     /**
      * Builds a theme using Grunt.
      *
-     * @param string|null $themeName the name of the theme to build.
-     * @return string the rendering result.
-     * @throws \yii\web\ForbiddenHttpException if the user is not allowed to perform this action.
-     * @throws \yii\web\ServerErrorHttpException if the working directory is invalid.
+     * @param string|null $themeName The name of the theme to build.
+     * @return string The rendering result.
      */
-    public function actionBuildTheme($themeName = null)
+    public function actionBuildTheme()
     {
-        try {
-            $gruntCommand = 'build-theme';
-            if ($themeName !== null) {
-                $gruntCommand .= ' --name=' . escapeshellarg($themeName);
-            }
-            $output = $this->executeGruntCommand($gruntCommand);
-            return $this->renderPartial('build-theme', ['output' => $output]);
-        } catch (\Exception $e) {
-            Yii::error('Error executing Grunt command: ' . $e->getMessage());
-            Yii::$app->session->setFlash('error', 'Error executing Grunt command: ' . $e->getMessage());
-            return $this->redirect(['index']);
+        $themeName = Yii::$app->request->post('themeName');
+
+        if ($themeName === null) {
+            throw new \yii\web\BadRequestHttpException('Theme name cannot be null.');
         }
+
+        $gruntService = new GruntService();
+        $output = $gruntService->buildTheme($themeName);
+
+        return $this->asJson(['success' => true, 'output' => $output]);
     }
 
     /**
      * Builds the search index using Grunt.
      *
-     * @return string the rendering result.
-     * @throws \yii\web\ForbiddenHttpException if the user is not allowed to perform this action.
-     * @throws \yii\web\ServerErrorHttpException if the working directory is invalid.
+     * @return string The rendering result.
      */
     public function actionBuildSearch()
     {
         try {
-            $output = $this->executeGruntCommand('build-search');
+            $output = $this->gruntService->buildSearch();
             return $this->renderPartial('build-search', ['output' => $output]);
         } catch (\Exception $e) {
             Yii::error('Error executing Grunt command: ' . $e->getMessage());
@@ -85,63 +86,18 @@ class GruntController extends Controller
     /**
      * Runs migrations using Grunt.
      *
-     * @param string|null $module the name of the module to migrate.
-     * @return string the rendering result.
-     * @throws \yii\web\ForbiddenHttpException if the user is not allowed to perform this action.
-     * @throws \yii\web\ServerErrorHttpException if the working directory is invalid.
+     * @param string|null $module The name of the module to migrate.
+     * @return string The rendering result.
      */
     public function actionMigrateUp($module = null)
     {
         try {
-            $gruntCommand = 'migrate-up';
-            if ($module !== null) {
-                $gruntCommand .= ' --module=' . escapeshellarg($module);
-            }
-            $output = $this->executeGruntCommand($gruntCommand);
+            $output = $this->gruntService->migrateUp($module);
             return $this->renderPartial('migrate-up', ['output' => $output]);
         } catch (\Exception $e) {
             Yii::error('Error executing Grunt command: ' . $e->getMessage());
             Yii::$app->session->setFlash('error', 'Error executing Grunt command: ' . $e->getMessage());
             return $this->redirect(['index']);
         }
-    }
-
-    /**
-     * Executes a Grunt command and returns the output.
-     *
-     * @param string $command the Grunt command to execute.
-     * @return array the output of the Grunt command.
-     * @throws \yii\web\ForbiddenHttpException if the user is not allowed to perform this action.
-     * @throws \yii\web\ServerErrorHttpException if the working directory is invalid.
-     */
-    private function executeGruntCommand($command)
-    {
-        $output = [];
-
-        if (!Yii::$app->user->isAdmin()) {
-            throw new \yii\web\ForbiddenHttpException('You are not allowed to perform this action.');
-        }
-
-        $workingDirectory = $_SERVER['DOCUMENT_ROOT'];
-
-        if (!is_dir($workingDirectory)) {
-            Yii::error('Invalid working directory: ' . $workingDirectory);
-            throw new \yii\web\ServerErrorHttpException('Invalid working directory.');
-        }
-
-        Yii::info('Current Working Directory: ' . $workingDirectory);
-
-        exec("cd $workingDirectory && grunt --gruntfile=$workingDirectory/Gruntfile.js $command 2>&1", $output, $returnCode);
-
-        Yii::info('Grunt Command executed: grunt --gruntfile=' . $workingDirectory . '/Gruntfile.js ' . $command);
-        Yii::info('Output: ' . print_r($output, true));
-        Yii::info('Return code: ' . $returnCode);
-
-        if ($returnCode !== 0) {
-            Yii::error('Failed to execute Grunt command: ' . $command . '. Return code: ' . $returnCode);
-            Yii::$app->session->setFlash('error', 'Failed to execute Grunt command: ' . $command . '. Return code: ' . $returnCode);
-        }
-
-        return $output;
     }
 }
