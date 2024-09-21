9fb942cdaf3fe392bfc1692e12bf0a5c5b07cb24
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-09-20 22:20:15 -0400

    Update IDEHelper.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- helpers/IDEHelper.php
+++ helpers/IDEHelper.php
@@ -10,13 +10,33 @@ use Yii;
  * This class provides IDE helper methods for better code autocompletion and type hinting in your IDE.
  */
 class IDEHelper
-{   
+{
+    /**
+     * Get the root directory of the application (webroot).
+     *
+     * @return string The root directory path.
+     */
+    public static function getRootPath(): string
+    {
+        return Yii::getAlias('@webroot');
+    }
+
+    /**
+     * Get the path to the root composer.json file.
+     *
+     * @return string The path to the root composer.json file.
+     */
+    public static function getRootComposerPath(): string
+    {
+        return self::getRootPath() . DIRECTORY_SEPARATOR . 'composer.json';
+    }
+
     /**
      * Get the path to the theme directory.
      *
      * @return string The path to the theme directory.
      */
-    public static function getThemePath()
+    public static function getThemePath(): string
     {
         return Yii::getAlias('@webroot/themes');
     }
@@ -27,20 +47,20 @@ class IDEHelper
      * @param string $themeName The name of the theme.
      * @return string The path to the theme's directory.
      */
-    public static function getThemeDirectoryPath($themeName)
+    public static function getThemeDirectoryPath(string $themeName): string
     {
-        return self::getThemePath() . DIRECTORY_SEPARATOR . $themeName; //@todo: Add wrapper for $themeName
+        return self::getThemePath() . DIRECTORY_SEPARATOR . $themeName;
     }
 
     /**
-     * Get the path to a specific theme's composer.json directory.
+     * Get the path to a specific theme's composer.json file.
      *
      * @param string $themeName The name of the theme.
-     * @return string The path to the theme's composer.json directory.
+     * @return string The path to the theme's composer.json file.
      */
-    public static function getThemeComposerPath($themeName)
+    public static function getThemeComposerPath(string $themeName): string
     {
-        return self::getThemeDirectoryPath($themeName) . 'composer.json';
+        return self::getThemeDirectoryPath($themeName) . DIRECTORY_SEPARATOR . 'composer.json';
     }
 
     /**
@@ -49,19 +69,19 @@ class IDEHelper
      * @param string $moduleName The name of the module.
      * @return string The path to the module's directory.
      */
-    public static function getModulePath($moduleName)
+    public static function getModulePath(string $moduleName): string
     {
-        return Yii::getAlias('@app/modules/' . $moduleName); // @todo: Add wrapper for $moduleName
+        return Yii::getAlias('@webroot/protected/modules/' . $moduleName);
     }
 
     /**
-     * Get the path to a specific module's composer.json directory.
+     * Get the path to a specific module's composer.json file.
      *
      * @param string $moduleName The name of the module.
-     * @return string The path to the module's composer.json directory.
+     * @return string The path to the module's composer.json file.
      */
-    public static function getModuleComposerPath($moduleName)
+    public static function getModuleComposerPath(string $moduleName): string
     {
-        return self::getModulePath($moduleName) . 'composer.json';
+        return self::getModulePath($moduleName) . DIRECTORY_SEPARATOR . 'composer.json';
     }
 }
